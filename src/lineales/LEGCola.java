60777d1071b3fdc460298eacda2ce6817efdd955
Pau <paula.navarro23@estudiantes.uva.es>
2025-04-24 11:07:25 +0200

    algo del chati

--- src/lineales/LEGCola.java
+++ src/lineales/LEGCola.java
@@ -1,4 +1,35 @@
 package lineales;
 
-public class LEGCola {
+import modelos.I_Cola;
+
+public class LEGCola<E> implements I_Cola<E> {
+    private LEGConUltimo<E> lista;
+
+    public LEGCola() {
+        lista = new LEGConUltimo<>();
+    }
+
+    public void encolar(E elem) {
+        lista.insertarFinal(elem);
+    }
+
+    public E desencolar() {
+        return lista.eliminarPrimero();
+    }
+
+    public E primero() {
+        return lista.primero();
+    }
+
+    public boolean esVacia() {
+        return lista.esVacia();
+    }
+
+    public void vaciar() {
+        lista.vaciar();
+    }
+
+    public String toString() {
+        return lista.toString();
+    }
 }
--- src/lineales/LEGPila.java
+++ src/lineales/LEGPila.java
@@ -1,4 +1,35 @@
 package lineales;
 
-public class LEGPila {
+import modelos.I_Pila;
+
+public class LEGPila<E> implements I_Pila<E> {
+    private LEG<E> lista;
+
+    public LEGPila() {
+        lista = new LEG<>();
+    }
+
+    public void apilar(E elem) {
+        lista.insertar(elem);
+    }
+
+    public E desapilar() {
+        return lista.eliminarPrimero();
+    }
+
+    public E cima() {
+        return lista.primero();
+    }
+
+    public boolean esVacia() {
+        return lista.esVacia();
+    }
+
+    public void vaciar() {
+        lista.vaciar();
+    }
+
+    public String toString() {
+        return lista.toString();
+    }
 }
